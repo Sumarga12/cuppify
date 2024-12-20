5db6a75e10f3d0ba5f37f290d89e2f2ffd118a13
Sumarga12 <fireb5388@gmail.com>
2024-12-20 13:02:23 +0545

    updated app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,29 @@
+import 'package:cuppify/core/app_theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'views/splash_screen.dart';
+
+import 'views/dashboard_screen.dart';
+import 'views/login_screen.dart';
 import 'views/onboarding_screen.dart';
 import 'views/signup_screen.dart';
-import 'views/login_screen.dart';
-import 'views/dashboard_screen.dart';
+import 'views/splash_screen.dart';
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Cuppify',
+      theme: getApplicationTheme(),
       initialRoute: '/splash',
       routes: {
         '/splash': (context) => SplashScreen(),
-        '/onboarding': (context) => OnboardingScreen(),
-        '/signup': (context) => SignUpScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/signup': (context) => const SignUpScreen(),
         '/login': (context) => LoginScreen(),
         '/dashboard': (context) => DashboardScreen(), // Add Dashboard route
       },
     );
   }
 }
-  
